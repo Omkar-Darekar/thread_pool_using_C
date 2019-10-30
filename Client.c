769af88f85f91556054d9b23a224b656ae1c92c5
Omkar Ashok Darekar <32523193+Omkar-Darekar@users.noreply.github.com>
2019-10-30 16:31:25 +0530

    Clinet.c
    Its Client side code. 
    Make sure you run this file after Server.c
    
    Command to Run -------->  gcc Client.c -o Client -lpthread
                                               ./Client

---
+++ Client.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include <netdb.h> 
+#include <stdio.h> 
+#include <arpa/inet.h>
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include<pthread.h>
+#define MAX 100
+#define SockAdd struct sockaddr
+#define PORT 8889
+#define max_thread 30
+
+int mainSock;
+struct sockaddr_in servInfo;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;	
+
+
+void func(int mainSock){
+	char msg[300];
+	char buf[300];
+
+	bzero(buf,sizeof(buf));
+	puts("\n------------------------  Listening from client side -------------- \n");
+	read(mainSock,buf,sizeof(buf));
+	
+	printf("Server message ---->> %s",buf);
+}
+
+
+void *mulClient(void *vargp){
+	pthread_mutex_lock(&lock);
+	mainSock= socket(AF_INET,SOCK_STREAM,0);
+
+	if(mainSock ==-1){
+		printf("\n Main Socket Failed \n");
+	}else{
+		printf("\n Main Socket Success \n");
+	}
+
+	bzero(&servInfo, sizeof(servInfo));
+	
+	servInfo.sin_family = AF_INET; 
+	servInfo.sin_addr.s_addr = INADDR_ANY; 
+	servInfo.sin_port = htons(PORT); 
+
+	if(connect(mainSock,(SockAdd*)&servInfo,sizeof(servInfo))!=0){
+		puts("\n Inside Clinet Connection Failed \n");
+		exit(0);	
+	}else{
+		puts("\n Inside Client Connect \n");
+		printf("mainSocket ==>> %d \n",mainSock);
+	}
+
+	func(mainSock);
+	pthread_mutex_unlock(&lock);
+
+}
+
+int main(){
+	int i;
+	pthread_t thread_id[max_thread];
+
+	for(i=0;i<max_thread;i++){
+		if((pthread_create(&thread_id[i],NULL,mulClient,NULL))!=0)
+			printf("\n Thread creation failed at ============ >>>>> %d \n ",i);
+		else 
+			printf("\n Thread successfully created at =======>> %d\n",i);
+	}
+
+	for(i=0;i<max_thread;i++){		
+		pthread_join(thread_id[i],NULL);
+		printf("\n after thread_id[%d] joining \n",i);
+	}
+
+return 0;
+}
